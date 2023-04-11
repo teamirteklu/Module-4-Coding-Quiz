7033e93de929be9368041c910e26b9dfeafea09c
tteklu <teamirteklu@gmail.com>
2023-04-11 13:35:43 -0600

    changes to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,64 @@
 var quizStarter = document.querySelector("#quizstart");
 var answerSelector = document.querySelector("div.container");
 
-var answerButton = document.querySelector("#answer");
-var answer2Button = document.querySelector("#answer2");
-var answer3Button = document.querySelector("#answer3");
-var answer4Button = document.querySelector("#answer4");
+var answerButton = document.querySelectorAll(".quizbutton");
+// var answer2Button = document.querySelector("#answer2");
+// var answer3Button = document.querySelector("#answer3");
+// var answer4Button = document.querySelector("#answer4");
 
 var quizBubble = document.querySelectorAll("div");
 var quizAnswer = document.querySelectorAll("input");
 var timeEl = document.querySelector(".time");
 var questionArray = ["question1", "question2", "question3", "question4"];
 var answerArray = ["answer1", "answer2", "answer3", "answer4"];
-var secondsLeft = 75;
+var secondsLeft = 66;
 var score = 0;
+
+// ----------Starting Button-----------------------------------
 
 quizStarter.addEventListener("click", function () {
 
-    setTime();
+  setTime();
 
-    var quizQuestion = document.getElementById("quizBegin");
-    var question = document.createElement("div");
-    question.setAttribute("id", "quizBegin");
-    var questiontxt = document.createTextNode(questionArray[0]);
-    question.appendChild(questiontxt);
-    quizQuestion.replaceChild(question, quizQuestion.childNodes[0]);
-
-
-
-    QuesAnswer(answerArray);
-    
+  var quizQuestion = document.getElementById("quizBegin");
+  var question = document.createElement("div");
+  question.setAttribute("id", "quizBegin");
+  var questiontxt = document.createTextNode(questionArray[0]);
+  question.appendChild(questiontxt);
+  quizQuestion.replaceChild(question, quizQuestion.childNodes[0]);
 
 
-
-    
-    
-    
+ 
+  QuesAnswer(answerArray);
+  buttonClick();
 });
+
+function buttonClick() {
+  for (let index = 0; index < answerButton.length; index++) {
+    console.log(index);
+    console.log(answerButton)
+  }
+}
+// answerButton[1].addEventListener("click", checkAnswer);
+
+    function checkAnswer() {
+      if (answerButton.value === "correct") {
+        score++
+        console.log(score);
+      }
+    }
+// function nextQues(array) {
+//   var i = 1++;
+//   var quizQuestion = document.getElementById("quizBegin");
+//   var question = document.createElement("div");
+//   question.setAttribute("id", "quizBegin");
+//   var questiontxt = document.createTextNode(array[i]);
+//   question.appendChild(questiontxt);
+//   quizQuestion.replaceChild(question, quizQuestion.childNodes[0]);
+// }
+
+// -------------------Set Time---------------------------------
+
 
 function setTime() {
     // Sets interval in variable
@@ -44,19 +67,39 @@ function setTime() {
       timeEl.textContent = secondsLeft + " seconds left";
   
       if(secondsLeft === 0) {
-        // Stops execution of action at set interval
         clearInterval(timerInterval);
-        // Calls function to create and append image
-        ;
+        endQuiz();
       }
-  
     }, 1000);
   }
+
+  // ------------------Leaderboard------------------------------
+
+  function endQuiz() {
+    var quizHeader = document.getElementById("quizBegin");
+    var question = document.createElement("div");
+    question.setAttribute("id", "quizBegin");
+    var headertxt = document.createTextNode("Leaderboard");
+    question.appendChild(headertxt);
+    quizHeader.replaceChild(question, quizHeader.childNodes[0]);
+
+    var quizAnswer = document.getElementsByClassName("quizbutton");
+    var initialInput = document.createElement("INPUT");
+    initialInput.setAttribute("type", "text");
+    document.body.appendChild(initialInput);
+    // quizAnswer.replaceChild(initialInput, quizAnswer.childNodes[0]);
+    console.log(quizAnswer);
+    
+  }
+
+  // ------------------------Score------------------------------
 
 
 function highScore(){
 
 }
+
+// -------------------------Answer Change-----------------------
 
 function QuesAnswer (array) {
     var quizAnswer = document.getElementById("innertext");
@@ -70,7 +113,7 @@ function QuesAnswer (array) {
 
     var quizContainer = document.getElementById("container");
     var answer2 = document.createElement("button");
-    answer2.setAttribute("id", "answer2");
+    answer2.setAttribute("id", "answer");
     answer2.setAttribute("value", "correct");
     answer2.setAttribute("class", "quizbutton");
     var txt2 = document.createTextNode(array[1]);
@@ -80,7 +123,7 @@ function QuesAnswer (array) {
 
     var quizContainer = document.getElementById("container");
     var answer3 = document.createElement("button");
-    answer3.setAttribute("id", "answer3");
+    answer3.setAttribute("id", "answer");
     answer3.setAttribute("class", "quizbutton");
     var txt3 = document.createTextNode(array[2]);
     answer3.appendChild(txt3);
@@ -89,7 +132,7 @@ function QuesAnswer (array) {
 
     var quizContainer = document.getElementById("container");
     var answer4 = document.createElement("button");
-    answer4.setAttribute("id", "answer4");
+    answer4.setAttribute("id", "answer");
     answer4.setAttribute("class", "quizbutton");
     var txt4 = document.createTextNode(array[3]);
     answer4.appendChild(txt4);
